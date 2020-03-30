882f7c740f543fc3b4b7d719048048aaf004c176
VojislavM <milivojevicvoja@yahoo.com>
2020-03-30 15:25:43 +0200

    tested new izo board v2.1

--- src/main.c
+++ src/main.c
@@ -85,7 +85,6 @@ void main(void)
 
 	k_sleep(500);
 	
-	
 /************************************TEST1************************************/
 /******************************LED and UART test******************************/
 	/*blink led in loop*/
@@ -99,7 +98,8 @@ void main(void)
 /*****************************************************************************/
 /*****************************************************************************/
 
-
+/************************************TEST2************************************/
+/**********************************I2C SCAN***********************************/
 	printk("Starting i2c scanner...\n");
 
 	/*start and configure i2c*/
@@ -135,57 +135,60 @@ void main(void)
 		
 		
 	}
+/*****************************************************************************/
+/*****************************************************************************/
 
-	struct device *dev = device_get_binding("HTS221");
-
-	if (dev == NULL) {
-		printk("Could not get HTS221 device\n");
-		return;
+/************************************TEST3************************************/
+/**********************************I2C ACCEL**********************************/
+	if (!init_accelerometer()) {
+		return -1;
 	}
 
-	if (IS_ENABLED(CONFIG_HTS221_TRIGGER)) {
-		struct sensor_trigger trig = {
-			.type = SENSOR_TRIG_DATA_READY,
-			.chan = SENSOR_CHAN_ALL,
-		};
-		if (sensor_trigger_set(dev, &trig, hts221_handler) < 0) {
-			printk("Cannot configure trigger\n");
-			return;
-		};
+	while (1) {
+		printk("\033[1;1H");
+		printk("\033[2J");
+
+		double x_accel = 0;
+		double y_accel = 0;
+		double z_accel = 0;
+		get_accelerometer_data(&x_accel, &y_accel, &z_accel);
+
+		printk("Acceleration values:\n");
+		printk("------------------------------------------------------------------------------------------------------\n");
+		printf("X acceleration: %lf (m/s^2), Y acceleration: %lf (m/s^2), Z acceleration: %lf (m/s^2)\n", x_accel, y_accel, z_accel);
+		printk("------------------------------------------------------------------------------------------------------");
+
+		k_sleep(K_MSEC(500));
 	}
-
-	while (!IS_ENABLED(CONFIG_HTS221_TRIGGER)) {
-		process_sample(dev);
-		k_sleep(K_MSEC(2000));
-	}
-	k_sleep(K_FOREVER);
-
-	// if (!init_accelerometer()) {
-	// return -1;
-	// }
-
-	// while (1) {
-	// 	printk("\033[1;1H");
-	// 	printk("\033[2J");
-
-	// 	double x_accel = 0;
-	// 	double y_accel = 0;
-	// 	double z_accel = 0;
-	// 	get_accelerometer_data(&x_accel, &y_accel, &z_accel);
-
-	// 	printk("Acceleration values:\n");
-	// 	printk("------------------------------------------------------------------------------------------------------\n");
-	// 	printf("X acceleration: %lf (m/s^2), Y acceleration: %lf (m/s^2), Z acceleration: %lf (m/s^2)\n", x_accel, y_accel, z_accel);
-	// 	printk("------------------------------------------------------------------------------------------------------");
-
-	// 	k_sleep(K_MSEC(500));
-	// }
-
+/*****************************************************************************/
+/*****************************************************************************/
 
 	// return 0;
 
 	/*******************************************/
-	
+	/*	struct device *dev = device_get_binding("HTS221");*/
+
+/*	if (dev == NULL) {*/
+/*		printk("Could not get HTS221 device\n");*/
+/*		return;*/
+/*	}*/
+
+/*	if (IS_ENABLED(CONFIG_HTS221_TRIGGER)) {*/
+/*		struct sensor_trigger trig = {*/
+/*			.type = SENSOR_TRIG_DATA_READY,*/
+/*			.chan = SENSOR_CHAN_ALL,*/
+/*		};*/
+/*		if (sensor_trigger_set(dev, &trig, hts221_handler) < 0) {*/
+/*			printk("Cannot configure trigger\n");*/
+/*			return;*/
+/*		};*/
+/*	}*/
+
+/*	while (!IS_ENABLED(CONFIG_HTS221_TRIGGER)) {*/
+/*		process_sample(dev);*/
+/*		k_sleep(K_MSEC(2000));*/
+/*	}*/
+/*	k_sleep(K_FOREVER);*/
 
 	// /*blink led in loop*/
 	// while (1) {
